45ed51a6853660b679b84a62beb7fb8f2b1af02a
Robert Clarke <Robert.Clarke@northgateplc.com>
2017-06-29 12:40:19 +0100

    init

---
+++ web/src/containers/App/index.js
@@ -0,0 +1,76 @@
+// @flow
+import React, { Component, PropTypes } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { authenticate, unauthenticate, logout } from '../../actions/session';
+import Home from '../Home';
+import NotFound from '../../components/NotFound';
+import Login from '../Login';
+import Signup from '../Signup';
+import MatchAuthenticated from '../../components/MatchAuthenticated';
+import RedirectAuthenticated from '../../components/RedirectAuthenticated';
+import Sidebar from '../../components/Sidebar';
+import Room from '../Room';
+
+type Props = {
+  authenticate: () => void,
+  unauthenticate: () => void,
+  isAuthenticated: boolean,
+  willAuthenticate: boolean,
+  logout: () => void,
+  currentUserRooms: Array,
+}
+
+class App extends Component {
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.props.authenticate();
+    } else {
+      this.props.unauthenticate();
+    }
+  }
+
+  static contextTypes = {
+    router: PropTypes.object,
+  }
+
+  handleLogout = router => this.props.logout(router);
+
+  props: Props
+
+  render() {
+    const { isAuthenticated, willAuthenticate, currentUserRooms } = this.props;
+    const authProps = { isAuthenticated, willAuthenticate };
+
+    return (
+      <BrowserRouter>
+          <div style={{ display: 'flex', flex: '1' }}>
+            {isAuthenticated &&
+              <Sidebar
+                rooms={currentUserRooms}
+                onLogoutClick={this.handleLogout}
+              />
+            }
+            <Switch>
+              <MatchAuthenticated exact path="/" component={Home} {...authProps}/>
+              <RedirectAuthenticated path="/login" component={Login} {...authProps}/>
+              <RedirectAuthenticated path="/signup" component={Signup} {...authProps} />
+              <MatchAuthenticated path="/r/:id" component={Room} {...authProps} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default connect(
+  state => ({
+    isAuthenticated: state.session.isAuthenticated,
+    willAuthenticate: state.session.willAuthenticate,
+    currentUserRooms: state.rooms.currentUserRooms,
+  }),
+  { authenticate, unauthenticate, logout }
+)(App);
---
+++ web/src/containers/Room/index.js
@@ -0,0 +1,108 @@
+// @flow
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { connectToChannel, leaveChannel, createMessage, loadOlderMessages } from '../../actions/room';
+import MessageList from '../../components/MessageList';
+import MessageForm from '../../components/MessageForm';
+import RoomNavbar from '../../components/RoomNavbar';
+import RoomSidebar from '../../components/RoomSidebar';
+
+type MessageType = {
+  id: number,
+}
+
+type Props = {
+  socket: any,
+  channel: any,
+  room: Object,
+  match: {
+    params: {
+      id: number,
+    }
+  },
+  connectToChannel: () => void,
+  leaveChannel: () => void,
+  createMessage: () => void,
+  messages: Array<MessageType>,
+  presentUsers: Array,
+  currentUser: Object,
+  loadingOlderMessages: boolean,
+  pagination: {
+    total_pages: number,
+    total_entries: number,
+    page_size: number,
+    page_number: number,
+  },
+  loadOlderMessages: () => void,
+}
+
+class Room extends Component {
+  componentDidMount() {
+    this.props.connectToChannel(this.props.socket, this.props.match.params.id);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.match.params.id !== this.props.match.params.id) {
+      this.props.leaveChannel(this.props.channel);
+      this.props.connectToChannel(nextProps.socket, nextProps.match.params.id);
+    }
+    if (!this.props.socket && nextProps.socket) {
+      this.props.connectToChannel(nextProps.socket, nextProps.match.params.id);
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.leaveChannel(this.props.channel);
+  }
+
+  props: Props
+
+  handleLoadMore = () =>
+    this.props.loadOlderMessages(
+      this.props.match.params.id,
+      { last_seen_id: this.props.messages[0].id }
+    )
+
+  handleMessageCreate = (data) => {
+    this.props.createMessage(this.props.channel, data);
+  }
+
+  render() {
+
+    const moreMessages = this.props.pagination.total_pages > this.props.pagination.page_number;
+
+    return (
+      <div style={{ display: 'flex', height: '100vh' }}>
+        <RoomSidebar
+          room={this.props.room}
+          currentUser={this.props.currentUser}
+          presentUsers={this.props.presentUsers}
+        />
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <RoomNavbar room={this.props.room} />
+          <MessageList
+            moreMessages={moreMessages}
+            messages={this.props.messages}
+            onLoadMore={this.handleLoadMore}
+            ref={(c) => { this.messageList = c; }}
+            loadingOlderMessages={this.props.loadingOlderMessages} />
+          <MessageForm onSubmit={this.handleMessageCreate} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({
+    room: state.room.currentRoom,
+    socket: state.session.socket,
+    channel: state.room.channel,
+    messages: state.room.messages,
+    presentUsers: state.room.presentUsers,
+    currentUser: state.session.currentUser,
+    pagination: state.room.pagination,
+    loadingOlderMessages: state.room.loadingOlderMessages,
+  }),
+  { connectToChannel, leaveChannel, createMessage, loadOlderMessages }
+)(Room);

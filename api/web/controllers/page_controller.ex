cc53852556a54a211e835e29156b9b970f59e089
Robert Clarke <Robert.Clarke@northgateplc.com>
2017-07-06 14:22:56 +0100

    push to master

--- api/web/controllers/page_controller.ex
+++ api/web/controllers/page_controller.ex
@@ -2,9 +2,6 @@ defmodule Sling.PageController do
   use Sling.Web, :controller
 
   def index(conn, _params) do
-        conn
-    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
-    |> put_flash(:error, "Let's pretend we have an error.")
-    |> render("index.html")
+    render conn, "index.html"
   end
 end
